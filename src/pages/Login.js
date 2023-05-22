ec5b8fa71deff20cbfde22c54abd020e18cd06cc
MauricioPassosF <mauricio.passos97@gmail.com>
2023-05-22 16:51:03 -0300

    Grupo 13-Req 1: Req ok

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import validator from 'validator';
 
 class Login extends Component {
+  state = {
+    emailInput: '',
+    nameInput: '',
+    isButtonDisabled: true,
+  };
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { emailInput, nameInput } = this.state;
+      this.setState({
+        isButtonDisabled: !(validator.isEmail(emailInput) && nameInput.length > 0),
+      });
+    });
+  };
+
   render() {
+    const { emailInput, nameInput, isButtonDisabled } = this.state;
     return (
-      <h1>Login</h1>
+      <div className="login-page">
+        <h1>Login</h1>
+        <form className="login-form">
+          <label htmlFor="emailInput">
+            <input
+              type="text"
+              data-testid="input-gravatar-email"
+              name="emailInput"
+              id="emailInput"
+              placeholder="Digite e-mail do Gravatar"
+              value={ emailInput }
+              onChange={ (e) => this.handleInput(e) }
+            />
+          </label>
+          <label htmlFor="nameInput">
+            <input
+              type="text"
+              data-testid="input-player-name"
+              name="nameInput"
+              id="nameInput"
+              placeholder="Digite seu nome"
+              value={ nameInput }
+              onChange={ (e) => this.handleInput(e) }
+            />
+          </label>
+          <Link to="/game">
+            <button
+              type="button"
+              id="play-button"
+              data-testid="btn-play"
+              disabled={ isButtonDisabled }
+              // onClick={ () => (dispatch(loginAction(emailInput))) }
+            >
+              JOGAR
+            </button>
+          </Link>
+        </form>
+      </div>
+
     );
   }
 }
