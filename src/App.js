d104cc19333025599882eed74ee4623877a59594
MauricioPassosF <mauricio.passos97@gmail.com>
2023-05-22 15:50:29 -0300

    Grupo 13-Req 0: Configuracao Redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router-dom';
+// import logo from './trivia.png';
 import './App.css';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import Ranking from './pages/Ranking';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={ logo } className="App-logo" alt="logo" />
+    //     <p>SUA VEZ</p>
+    //   </header>
+    // </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/game" component={ Game } />
+      <Route exact path="/ranking" component={ Ranking } />
+      <Route exact path="/feedback" component={ Feedback } />
+    </Switch>
   );
 }
